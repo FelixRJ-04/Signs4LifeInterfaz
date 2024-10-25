fad4f8f25d7b8458b4bda6140687f87f7a23684b
FelixRJ-04 <rios.felix10slp@gmail.com>
2024-10-25 11:10:36 -0600

    k

--- Signs4Life/Form1.Designer.cs
+++ Signs4Life/Form1.Designer.cs
@@ -43,10 +43,10 @@
             // Logo
             // 
             Logo.Image = Properties.Resources.ls4l_removebg_preview;
-            Logo.Location = new Point(792, 16);
-            Logo.Margin = new Padding(5, 4, 3, 5);
+            Logo.Location = new Point(693, 12);
+            Logo.Margin = new Padding(4, 3, 3, 4);
             Logo.Name = "Logo";
-            Logo.Size = new Size(87, 89);
+            Logo.Size = new Size(76, 67);
             Logo.SizeMode = PictureBoxSizeMode.Zoom;
             Logo.TabIndex = 0;
             Logo.TabStop = false;
@@ -55,9 +55,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Lucida Sans", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(510, 132);
+            label1.Location = new Point(446, 99);
             label1.Name = "label1";
-            label1.Size = new Size(270, 23);
+            label1.Size = new Size(211, 18);
             label1.TabIndex = 1;
             label1.Text = "Traducción de LSM a texto";
             label1.Click += label1_Click;
@@ -66,10 +66,9 @@
             // 
             button1.BackColor = Color.LightPink;
             button1.ForeColor = Color.Black;
-            button1.Location = new Point(564, 292);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(494, 219);
             button1.Name = "button1";
-            button1.Size = new Size(216, 47);
+            button1.Size = new Size(189, 35);
             button1.TabIndex = 3;
             button1.Text = "Responder";
             button1.UseVisualStyleBackColor = false;
@@ -77,10 +76,9 @@
             // button2
             // 
             button2.BackColor = Color.LightGreen;
-            button2.Location = new Point(77, 461);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(67, 346);
             button2.Name = "button2";
-            button2.Size = new Size(158, 55);
+            button2.Size = new Size(138, 41);
             button2.TabIndex = 4;
             button2.Text = "Iniciar traducción";
             button2.UseVisualStyleBackColor = false;
@@ -88,10 +86,9 @@
             // button3
             // 
             button3.BackColor = Color.LightGreen;
-            button3.Location = new Point(271, 461);
-            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Location = new Point(237, 346);
             button3.Name = "button3";
-            button3.Size = new Size(158, 55);
+            button3.Size = new Size(138, 41);
             button3.TabIndex = 5;
             button3.Text = "Detener traducción";
             button3.UseVisualStyleBackColor = false;
@@ -99,43 +96,45 @@
             // 
             // Sound
             // 
+            Sound.BackColor = Color.Transparent;
             Sound.BackgroundImage = (Image)resources.GetObject("Sound.BackgroundImage");
             Sound.BackgroundImageLayout = ImageLayout.Stretch;
-            Sound.Location = new Point(670, 511);
-            Sound.Margin = new Padding(3, 4, 3, 4);
+            Sound.Location = new Point(586, 383);
             Sound.Name = "Sound";
-            Sound.Size = new Size(67, 60);
+            Sound.Padding = new Padding(10, 2, 2, 10);
+            Sound.Size = new Size(59, 45);
             Sound.TabIndex = 6;
             Sound.Text = "\r\n";
-            Sound.UseVisualStyleBackColor = true;
+            Sound.UseVisualStyleBackColor = false;
             // 
             // Settings
             // 
+            Settings.BackColor = Color.Transparent;
             Settings.BackgroundImage = Properties.Resources.Conf_removebg_preview;
             Settings.BackgroundImageLayout = ImageLayout.Stretch;
-            Settings.Location = new Point(760, 511);
-            Settings.Margin = new Padding(3, 4, 3, 4);
+            Settings.Location = new Point(665, 383);
             Settings.Name = "Settings";
-            Settings.Size = new Size(70, 60);
+            Settings.Size = new Size(61, 45);
             Settings.TabIndex = 7;
-            Settings.UseVisualStyleBackColor = true;
+            Settings.UseVisualStyleBackColor = false;
             // 
             // LBLSM
             // 
             LBLSM.BackColor = Color.LightPink;
             LBLSM.FormattingEnabled = true;
-            LBLSM.ItemHeight = 20;
-            LBLSM.Location = new Point(510, 167);
+            LBLSM.ItemHeight = 15;
+            LBLSM.Location = new Point(446, 125);
+            LBLSM.Margin = new Padding(3, 2, 3, 2);
             LBLSM.Name = "LBLSM";
-            LBLSM.Size = new Size(327, 104);
+            LBLSM.Size = new Size(287, 79);
             LBLSM.TabIndex = 8;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkSeaGreen;
-            ClientSize = new Size(914, 600);
+            ClientSize = new Size(800, 450);
             Controls.Add(LBLSM);
             Controls.Add(Settings);
             Controls.Add(Sound);
@@ -144,7 +143,6 @@
             Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(Logo);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Traductor de LSM";
             ((System.ComponentModel.ISupportInitialize)Logo).EndInit();
